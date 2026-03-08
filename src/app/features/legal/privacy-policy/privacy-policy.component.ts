da5374e7cc6444226cafd16fa296d1b1a72d8d83
AfonsoPaiva <afonsecapaiva@gmail.com>
2026-03-08 14:56:57 +0000

    socials and contacts

--- src/app/features/legal/privacy-policy/privacy-policy.component.ts
+++ src/app/features/legal/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class PrivacyPolicyComponent {
   today = new Date();
+  private readonly email = 'skillbridge.portfolio@gmail.com';
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  copyEmail(): void {
+    navigator.clipboard.writeText(this.email).then(() => {
+      this.snackBar.open('Email copiado com sucesso!', 'Fechar', { duration: 3000 });
+    }).catch(() => {
+      this.snackBar.open('Erro ao copiar email', 'Fechar', { duration: 3000 });
+    });
+  }
 }
