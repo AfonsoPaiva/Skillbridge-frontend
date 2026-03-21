2a187d5a090d7a46614ecea67292ac74ab5ab6e7
AfonsoPaiva <afonsecapaiva@gmail.com>
2026-03-21 20:01:26 +0000

    fixed-publickeys for notifications and scss for login and onboarding

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   firebaseProjectId: 'skill-bridge-makelinks',
   firebaseMessagingSenderId: '539786041322',
   firebaseAppId: '1:539786041322:web:91f796d509585b42f94493',
-  firebaseVapidKey: '',
+  firebaseVapidKey: 'BDQBzZVFjzZ_iu9f9prxrCBYmO4Y3_2d6WqOhXdeO5gOFy-b3VUUTbN4Tx6sWGBHNBJiKwaKH_jNWfz6kNA2p6Q',
   apiUrl: 'https://backendskillbridge-742354947031.europe-west1.run.app/api',
   stripePublishableKey: 'pk_live_51T5ZPbRhy1hjUWSqhNuABcC00PzHZ71kL9GaOqr3jzSSvcFjB4WdOyeo56HjumDtXR7iXCyoGCmL5zc2Cb8zFnm100MSgYYceG',
   stripeDonationURL: 'https://donate.stripe.com/test_aFacN44fz2pf5N1fPAenS00'
